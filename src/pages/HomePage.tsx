adc67b1d4c98a8395aa21a0af64562da8708ebdd
Lars Bosker <59372683+Lars-Bosker@users.noreply.github.com>
2022-12-21 20:15:17 +0100

    Change text

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,28 +5,35 @@ import {ITodoData} from "../services/ITodoData";
 
 function HomePage() {
     const config = {headers: {'Content-type': "multipart/form-data"}};
+    const url: string = "http://127.0.0.1:8000/api/todolist";
     const [data, setData] = useState<ITodoData[]>(Object);
     const fData = new FormData();
+
+    // Data that will be sent to API
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [completed, setCompleted] = useState(false);
     const [image, setImage] = useState("");
-    const url: string = "http://127.0.0.1:8000/api/todolist";
 
-    const RequestTodoData = () => {
+    /**
+     * Function for getting all todo-items
+     * @constructor
+     */
+    function RequestTodoData() {
         axios.get(url)
             .then(res => {
                 setData(res.data['data']);
             })
     }
 
+    /**
+     * Function for sending data for new todo-item to api
+     * @param url
+     */
     function postData(url: string) {
         axios.post(url, fData, config)
             .then(function (res) {
                 setData(res.data['data']);
-            })
-            .catch(function (res) {
-                console.log(res)
             })
     }
 
@@ -35,8 +42,8 @@ function HomePage() {
         []
     )
 
-    const submitFunction = (e: any) => {
-        // e.preventDefault();
+
+    function submitFunction() {
         fData.append("title", title)
         fData.append("description", description)
         fData.append("completed", JSON.stringify(completed))
@@ -47,7 +54,7 @@ function HomePage() {
     return (
         <div className="home">
             <div className="add-todo">
-                <h1>Add Todo</h1>
+                <h1>Todo toevoegen</h1>
                 <form onSubmit={submitFunction}>
                     <input id={"title"} type="text" placeholder={"Titel"} onChange={event => setTitle(event.target.value)}/>
                     <textarea id={"description"} placeholder={"Beschrijving"} onChange={event => setDescription(event.target.value)}/>
